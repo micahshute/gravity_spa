a16478655eb2d132177e3e723abb40fa6b623f0b
Micah Shute <micah.shute@gmail.com>
2019-12-02 19:05:54 -0800

    switch comps

--- frontend/src/components/mass.js
+++ frontend/src/components/mass.js
@@ -4,8 +4,29 @@ class Mass{
         return MassAdapter.instance
     }
 
-    constructor(params){
-        
+    static async retrieveAll(){
+        try{
+            const json = await this.adapter.getMasses()
+            return json.map(obj => new this(obj))
+        }catch{
+            return null
+        }
+    }
 
+    constructor(params){
+        const { id, name, radius, mass } = params
+        this.id = id
+        this.name = name
+        this.radius = radius
+        this.mass = mass
+    }
+
+
+    get html(){
+        return (`
+            <div class='circle'>
+
+            </div>
+        `)
     }
 }
